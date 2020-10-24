8529b951363941e4a34e52d67571abc223a88597
Konstantin Cherepanov <koncherepanov@gmail.com>
2020-10-24 12:02:26 +0500

    initial commit

---
+++ src/main/java/ru/dilgorp/java/LuckyTicketsREST/LuckyTicketsRestApplication.java
@@ -0,0 +1,12 @@
+package ru.dilgorp.java.LuckyTicketsREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyTicketsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyTicketsRestApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/dilgorp/java/LuckyTicketsREST/LuckyTicketsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dilgorp.java.LuckyTicketsREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyTicketsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
